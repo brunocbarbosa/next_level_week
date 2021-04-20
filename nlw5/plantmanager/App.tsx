ad281cc2963fa26ecbb3fb8aef9905a156d031e7
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2021-04-19 21:46:07 -0300

    react native class1 finished

---
+++ nlw5/plantmanager/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Welcome } from './src/pages/welcome';
+
+export default function App() {
+  return (
+   <Welcome />
+  );
+}
