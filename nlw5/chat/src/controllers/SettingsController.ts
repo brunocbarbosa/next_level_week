1234f510713179fb890af013e174f9b813debb74
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2021-04-23 16:22:57 -0300

    node class 3 16:57

--- nlw5/chat/src/controllers/SettingsController.ts
+++ nlw5/chat/src/controllers/SettingsController.ts
@@ -1,21 +1,23 @@
 import {Request, Response} from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SettingsRepository } from '../repositories/SettingsRepository';
+import { SettingsService } from '../services/SettingsService';
 
 class SettingsController{
   async create(req: Request, res: Response){
     const { chat, username } = req.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
+    const settingService = new SettingsService();
+
+    try {
+      const settings = await settingService.create({ chat, username })
   
-    const settings = settingsRepository.create({
-      chat,
-      username
-    })
-  
-    await settingsRepository.save(settings);
-  
-    return res.json(settings)
+      return res.json(settings)
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message
+      })
+    }
   }
 }
 
