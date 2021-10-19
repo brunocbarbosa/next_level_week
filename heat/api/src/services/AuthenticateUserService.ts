a39c69619627e6acc58b900e59b60bf46a35eec1
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2021-10-18 23:28:40 -0300

    class1 - 59:54

---
+++ heat/api/src/services/AuthenticateUserService.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+interface IAccessTokenResponse{
+  access_token: string
+}
+
+interface IUserResponse{
+  avatar_url: string,
+  login: string,
+  id: number,
+  number: string
+}
+
+class AuthenticateUserService{
+  async execute(code: string){
+    const url = 'https://github.com/login/oauth/access_token';
+
+    const { data: accesTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      }, 
+      headers:{
+        Accept: "application/json"
+      }
+    });
+
+    const res = await axios.get<IUserResponse>('https://api.github.com/user', {
+      headers:{
+        authorization:`Bearer ${accesTokenResponse.access_token}`
+      }
+    })
+
+    return res.data;
+  }
+}
+
+export { AuthenticateUserService }
